3db71e3bebd82fde5eb446f9b844822398c9e046
Joseph Weinkam <joseph.m.weinkam@gmail.com>
2024-04-12 11:17:51 -0700

    add NSwag and configure OpenAPI

--- drr/src/API/EMCR.DRR/Program.cs
+++ drr/src/API/EMCR.DRR/Program.cs
@@ -1,15 +1,46 @@
 ﻿using EMCR.DRR.Dynamics;
 using EMCR.Utilities;
+using NSwag;
+using NSwag.AspNetCore;
+using NSwag.Generation.Processors.Security;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddCache(string.Empty)
     .AddDRRDynamics(builder.Configuration);
+
+builder.Services.Configure<OpenApiDocumentMiddlewareSettings>(options =>
+{
+    options.Path = "/api/openapi/{documentName}/openapi.json";
+    options.DocumentName = "DRR API";
+    options.PostProcess = (document, req) =>
+    {
+        document.Info.Title = "DRR API";
+    };
+});
+
+builder.Services.Configure<SwaggerUi3Settings>(options =>
+{
+    options.Path = "/api/openapi";
+    options.DocumentTitle = "DRR API Documentation";
+    options.DocumentPath = "/api/openapi/{documentName}/openapi.json";
+});
+
+builder.Services.AddOpenApiDocument(document =>
+{
+    document.AddSecurity("bearer token", Array.Empty<string>(), new OpenApiSecurityScheme
+    {
+        Type = OpenApiSecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "paste token here",
+        In = OpenApiSecurityApiKeyLocation.Header
+    });
+
+    document.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("bearer token"));
+    document.GenerateAbstractProperties = true;
+});
 
 
 var app = builder.Build();
@@ -17,8 +48,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseOpenApi();
+    app.UseSwaggerUi3();
 }
 
 app.UseHttpsRedirection();
