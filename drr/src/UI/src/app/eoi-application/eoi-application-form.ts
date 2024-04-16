b6c684cbadd540ca28815bd0dfa9ef6fe1c8daf4
Vlad Belukha <vlad.belukha@quartech.com>
2024-04-16 11:38:53 -0700

    funding calculation; build bugdet fixed;

--- drr/src/UI/src/app/eoi-application/eoi-application-form.ts
+++ drr/src/UI/src/app/eoi-application/eoi-application-form.ts
@@ -1,5 +1,23 @@
-import { disable, email, minLength, prop, propArray, propObject, required } from "@rxweb/reactive-form-validators";
-import { ApplicantType, AreaUnits, ContactDetails, EOIApplication, FundingInformation, FundingType, Hazards, LocationInformation, ProjectType } from "../../model";
+import {
+  disable,
+  email,
+  minLength,
+  prop,
+  propArray,
+  propObject,
+  required,
+} from '@rxweb/reactive-form-validators';
+import {
+  ApplicantType,
+  AreaUnits,
+  ContactDetails,
+  EOIApplication,
+  FundingInformation,
+  FundingType,
+  Hazards,
+  LocationInformation,
+  ProjectType,
+} from '../../model';
 
 export class FundingInformationForm implements FundingInformation {
   @prop()
@@ -10,154 +28,160 @@ export class FundingInformationForm implements FundingInformation {
   type?: FundingType;
 
   constructor(values: FundingInformationForm) {
-    Object.assign(this, values);  
+    Object.assign(this, values);
   }
 }
 
 export class LocationInformationForm implements LocationInformation {
-  @prop()  
+  @prop()
   area?: string;
 
   @prop()
   areaUnits?: AreaUnits;
 
   @prop()
-  description?: string;  
+  description?: string;
 
   @prop()
-  ownership?: string;  
+  ownership?: string;
 
   constructor(values: LocationInformationForm) {
-    Object.assign(this, values);  
+    Object.assign(this, values);
   }
 }
 
 export class ContactDetailsForm implements ContactDetails {
-    @prop()
-    @required()
-    @email()
-    email?: string;
+  @prop()
+  @required()
+  @email()
+  email?: string;
 
-    @prop()
-    @required()
-    firstName?: string;
+  @prop()
+  @required()
+  firstName?: string;
 
-    @prop()
-    @required()
-    lastName?: string;
+  @prop()
+  @required()
+  lastName?: string;
 
-    @prop()
-    @required()
-    phone?: string;
+  @prop()
+  @required()
+  phone?: string;
 
-    @prop()
-    position?: string; // TODO: use as department for now
+  @prop()
+  position?: string; // TODO: use as department for now
 
-    @prop()
-    title?: string;
-  
-    constructor(values: ContactDetailsForm) {
-      Object.assign(this, values);  
-    }
-}
-  
-export class EOIApplicationForm implements EOIApplication {
-    @prop()
-    @required()
-    applicantName?: string;
-  
-    @prop()
-    @required()
-    applicantType?: ApplicantType;
-    
-    @prop()
-    area?: number;
-    
-    @prop()
-    backgroundDescription?: string;
-    
-    @prop()
-    cfoConfirmation?: boolean;
-    
-    @prop()
-    climateAdaptation?: string;  
-    
-    @prop()
-    coordinates?: string;
-    
-    @prop()
-    endDate?: string;
-    
-    @prop()
-    engagementProposal?: string;
-    
-    @prop()
-    foippaConfirmation?: boolean;
-    
-    @prop()
-    fundingRequest?: number;
-    
-    @prop()
-    identityConfirmation?: boolean;
-    
-    @prop()
-    locationDescription?: string;
+  @prop()
+  title?: string;
 
-    @propObject(LocationInformationForm)
-    locationInformation?: LocationInformationForm = new LocationInformationForm({});
-    
-    @propArray(FundingInformationForm)
-    otherFunding?: FundingInformationForm[] = [{}];
-    
-    @prop()
-    otherInformation?: string;  
-    
-    @prop()
-    ownership?: string;
-    
-    @prop()
-    ownershipDeclaration?: boolean;
-
-    @propArray(ContactDetailsForm)    
-    projectContacts?: ContactDetailsForm[] = [{}];
-    
-    @prop()
-    projectTitle?: string;
-    
-    @prop()
-    projectType?: ProjectType;
-    
-    @prop()
-    proposedSolution?: string;
-    
-    @prop()
-    rationaleForFunding?: string;
-    
-    @prop()
-    rationaleForSolution?: string;
-    
-    @prop()
-    reasonsToSecureFunding?: string;
-
-    @prop()
-    relatedHazards?: Hazards[];    
-    
-    @prop()
-    startDate?: string;
-
-    @propObject(ContactDetailsForm)
-    submitter?: ContactDetailsForm = new ContactDetailsForm({});
-    
-    @prop()
-    // @disable({ conditionalExpression: () => { return true; } })
-    totalFunding?: number;
-    
-    @prop()
-    unfundedAmount?: number;  
-    
-    @prop()
-    units?: string;
-
-    @prop()
-    otherHazardsDescription?: string;
+  constructor(values: ContactDetailsForm) {
+    Object.assign(this, values);
   }
+}
+
+export class EOIApplicationForm implements EOIApplication {
+  @prop()
+  @required()
+  applicantName?: string;
+
+  @prop()
+  @required()
+  applicantType?: ApplicantType;
+
+  @prop()
+  area?: number;
+
+  @prop()
+  backgroundDescription?: string;
+
+  @prop()
+  cfoConfirmation?: boolean;
+
+  @prop()
+  climateAdaptation?: string;
+
+  @prop()
+  coordinates?: string;
+
+  @prop()
+  endDate?: string;
+
+  @prop()
+  engagementProposal?: string;
+
+  @prop()
+  foippaConfirmation?: boolean;
+
+  @prop()
+  fundingRequest?: number;
+
+  @prop()
+  identityConfirmation?: boolean;
+
+  @prop()
+  locationDescription?: string;
+
+  @propObject(LocationInformationForm)
+  locationInformation?: LocationInformationForm = new LocationInformationForm(
+    {}
+  );
+
+  @propArray(FundingInformationForm)
+  otherFunding?: FundingInformationForm[] = [{}];
+
+  @prop()
+  otherInformation?: string;
+
+  @prop()
+  ownership?: string;
+
+  @prop()
+  ownershipDeclaration?: boolean;
+
+  @propArray(ContactDetailsForm)
+  projectContacts?: ContactDetailsForm[] = [{}];
+
+  @prop()
+  projectTitle?: string;
+
+  @prop()
+  projectType?: ProjectType;
+
+  @prop()
+  proposedSolution?: string;
+
+  @prop()
+  rationaleForFunding?: string;
+
+  @prop()
+  rationaleForSolution?: string;
+
+  @prop()
+  reasonsToSecureFunding?: string;
+
+  @prop()
+  relatedHazards?: Hazards[];
+
+  @prop()
+  startDate?: string;
+
+  @propObject(ContactDetailsForm)
+  submitter?: ContactDetailsForm = new ContactDetailsForm({});
+
+  @prop()
+  @disable({
+    conditionalExpression: () => {
+      return true;
+    },
+  })
+  totalFunding?: number;
+
+  @prop()
+  unfundedAmount?: number;
+
+  @prop()
+  units?: string;
+
+  @prop()
+  otherHazardsDescription?: string;
+}
--- drr/src/UI/src/app/step-3/step-3.component.ts
+++ drr/src/UI/src/app/step-3/step-3.component.ts
@@ -10,6 +10,7 @@ import { FormArray, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'drr-step-3',
@@ -32,6 +33,46 @@ export class Step3Component {
 
   formBuilder = inject(RxFormBuilder);
 
+  ngOnInit() {
+    this.eoiApplicationForm
+      .get('otherFunding')!
+      .valueChanges.pipe(distinctUntilChanged())
+      .subscribe(() => {
+        this.calculateTotalFunding();
+      });
+
+    this.eoiApplicationForm
+      .get('fundingRequest')!
+      .valueChanges.pipe(distinctUntilChanged())
+      .subscribe(() => {
+        this.calculateTotalFunding();
+      });
+
+    this.eoiApplicationForm
+      .get('unfundedAmount')!
+      .valueChanges.pipe(distinctUntilChanged())
+      .subscribe(() => {
+        this.calculateTotalFunding();
+      });
+  }
+
+  calculateTotalFunding() {
+    const otherFundingSum = this.getFormArray('otherFunding').controls.reduce(
+      (total, funding) => total + funding.value.amount,
+      0
+    );
+
+    const fundingRequest =
+      this.eoiApplicationForm.get('fundingRequest')?.value ?? 0;
+
+    const unfundedAmount =
+      this.eoiApplicationForm.get('unfundedAmount')?.value ?? 0;
+
+    const totalFunding = otherFundingSum + fundingRequest + unfundedAmount;
+
+    this.eoiApplicationForm.patchValue({ totalFunding });
+  }
+
   getFormArray(formArrayName: string) {
     return this.eoiApplicationForm.get(formArrayName) as FormArray;
   }
@@ -41,5 +82,9 @@ export class Step3Component {
       this.formBuilder.formGroup(FundingInformationForm),
       { emitEvent: false }
     );
+  }
+
+  removeOtherSource(index: number) {
+    this.getFormArray('otherFunding').removeAt(index);
   }
 }
