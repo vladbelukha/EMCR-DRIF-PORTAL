8ec1d1957fe844384687398eac05242f2c46750d
Vlad Belukha <vlad.belukha@quartech.com>
2024-04-24 12:03:21 -0700

    contact form component fixed

--- drr/src/UI/src/app/contact-details/contact-details.component.spec.ts
+++ drr/src/UI/src/app/contact-details/contact-details.component.spec.ts
@@ -1,23 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ContactDetailsComponent } from './contact-details.component';
+import { DrrContactDetailsComponent } from './contact-details.component';
 
 describe('ContactDetailsComponent', () => {
-  let component: ContactDetailsComponent;
-  let fixture: ComponentFixture<ContactDetailsComponent>;
+  let component: DrrContactDetailsComponent;
+  let fixture: ComponentFixture<DrrContactDetailsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ContactDetailsComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(ContactDetailsComponent);
+      imports: [DrrContactDetailsComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(DrrContactDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
   });
 });
--- drr/src/UI/src/app/contact-details/contact-details.component.ts
+++ drr/src/UI/src/app/contact-details/contact-details.component.ts
@@ -1,17 +1,33 @@
 import { Component, Input, input } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ContactDetailsForm } from '../eoi-application/eoi-application-form';
-import { RxFormGroup } from '@rxweb/reactive-form-validators';
+import { RxFormControl, RxFormGroup } from '@rxweb/reactive-form-validators';
 import { MatInputModule } from '@angular/material/input';
+import { DrrInputComponent } from '../drr-input/drr-input.component';
+import { TranslocoModule } from '@ngneat/transloco';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'drr-contact-details',
   standalone: true,
-  imports: [MatInputModule, MatFormFieldModule],
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    DrrInputComponent,
+    TranslocoModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './contact-details.component.html',
-  styleUrl: './contact-details.component.scss'
+  styleUrl: './contact-details.component.scss',
 })
-export class ContactDetailsComponent {
+export class DrrContactDetailsComponent {
   @Input()
   contactForm?: RxFormGroup;
+
+  getFormControl(name: string): RxFormControl {
+    return this.contactForm?.get(name) as RxFormControl;
+  }
 }
