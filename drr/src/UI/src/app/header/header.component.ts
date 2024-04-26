f353ba722480885690fc09aff86bd4c6902b1cc2
Vlad Belukha <vlad.belukha@quartech.com>
2024-04-26 12:19:42 -0700

    header title fixed

--- drr/src/UI/src/app/header/header.component.ts
+++ drr/src/UI/src/app/header/header.component.ts
@@ -1,16 +1,23 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatMenuModule } from '@angular/material/menu';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @Component({
   selector: 'drr-header',
   standalone: true,
-  imports: [CommonModule, MatDividerModule,  MatToolbarModule, MatMenuModule],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatMenuModule,
+    TranslocoModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HeaderComponent {
   homeClick() {}
