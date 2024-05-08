a8fa2c55d4e044307e605d2f3799165deb16efca
Joseph Weinkam <joseph.m.weinkam@gmail.com>
2024-05-08 15:39:43 -0700

    [DRR-136] API: move version endpoint to new controller

---
+++ drr/src/API/EMCR.DRR/Controllers/VersionController.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Mime;
+using System.Reflection;
+using EMCR.DRR.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMCR.DRR.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
+    public class VersionController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<VersionInformation>>> GetVersionInformation()
+        {
+            await Task.CompletedTask;
+            var version = Environment.GetEnvironmentVariable("VERSION") ?? string.Empty;
+            var name = Assembly.GetEntryAssembly()?.GetName().Name ?? string.Empty;
+            return Ok(new[] { new VersionInformation { Version = version, Name = name } });
+        }
+    }
+
+    public class VersionInformation
+    {
+        public required string Name { get; set; }
+        public required string Version { get; set; }
+    }
+}
