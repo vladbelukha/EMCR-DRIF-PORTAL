359cea43f6cc590b0609e8dca4abe8d03fe9f137
vladbelukha <vlad.beluha@gmail.com>
2024-08-08 12:54:21 -0700

    [DRR-540] 403 error message

--- drr/src/UI/src/app/core/interceptors/error.interceptor.ts
+++ drr/src/UI/src/app/core/interceptors/error.interceptor.ts
@@ -1,5 +1,7 @@
 import { HttpEvent, HttpHandlerFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { HotToastService } from '@ngneat/hot-toast';
 import { Observable, catchError, throwError } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
 
@@ -8,13 +10,23 @@ export const ErrorInterceptor = (
   next: HttpHandlerFn
 ): Observable<HttpEvent<any>> => {
   const authService = inject(AuthService);
+  const hotToast = inject(HotToastService);
+  const router = inject(Router);
 
   return next(req).pipe(
     catchError((err) => {
-      if (err.status === 401) {
-        // TODO: maybe API auth have different timeout or smth?
-        // authService.logout();
-        // authService.login();
+      switch (err.status) {
+        // case 401:
+        //   hotToast.error(
+        //     'You are not authenticated. Please try logging in again.'
+        //   );
+        //   break;
+        case 403:
+          router.navigate(['/']);
+          hotToast.error('You do not have permission to access this resource.');
+          break;
+        default:
+          break;
       }
       return throwError(err);
     })
