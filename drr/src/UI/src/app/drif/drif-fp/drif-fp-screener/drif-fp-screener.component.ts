a0635ed5e559aade6c2daa8cb48c5930616dd57c
vladbelukha <vlad.beluha@gmail.com>
2024-09-24 14:17:49 -0700

    [DRR-897] instructions text changed, header divider added; screener header divider added.

--- drr/src/UI/src/app/drif/drif-fp/drif-fp-screener/drif-fp-screener.component.ts
+++ drr/src/UI/src/app/drif/drif-fp/drif-fp-screener/drif-fp-screener.component.ts
@@ -3,6 +3,7 @@ import { Component, inject } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
@@ -36,6 +37,7 @@ import { DrifFpScreenerQuestionComponent } from './drif-fp-screener-question.com
     MatIconModule,
     MatListModule,
     MatCardModule,
+    MatDividerModule,
     DrrRadioButtonComponent,
     DrifFpScreenerQuestionComponent,
   ],
