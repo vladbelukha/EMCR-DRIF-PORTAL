05093f28383a25f732abd2cf4b07e02f05ed430b
vladbelukha <vlad.beluha@gmail.com>
2025-01-03 12:49:06 -0800

    [DRR-1455] basic create forecast layout

--- drr/src/UI/src/app/drif/drif-project/drif-forecast/drif-forecast-create/drif-forecast-create.component.ts
+++ drr/src/UI/src/app/drif/drif-project/drif-forecast/drif-forecast-create/drif-forecast-create.component.ts
@@ -1,12 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import {
+  MatStepperModule,
+  StepperOrientation,
+} from '@angular/material/stepper';
+import { TranslocoModule } from '@ngneat/transloco';
+import { DrrCurrencyInputComponent } from '../../../../shared/controls/drr-currency-input/drr-currency-input.component';
+import { DrrDatepickerComponent } from '../../../../shared/controls/drr-datepicker/drr-datepicker.component';
+import { DrrInputComponent } from '../../../../shared/controls/drr-input/drr-input.component';
+import { DrrRadioButtonComponent } from '../../../../shared/controls/drr-radio-button/drr-radio-button.component';
+import { DrrSelectComponent } from '../../../../shared/controls/drr-select/drr-select.component';
+import { DrrTextareaComponent } from '../../../../shared/controls/drr-textarea/drr-textarea.component';
 
 @Component({
   selector: 'drr-drif-forecast-create',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatStepperModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    TranslocoModule,
+    DrrDatepickerComponent,
+    DrrInputComponent,
+    DrrSelectComponent,
+    DrrRadioButtonComponent,
+    DrrTextareaComponent,
+    DrrCurrencyInputComponent,
+  ],
   templateUrl: './drif-forecast-create.component.html',
-  styleUrl: './drif-forecast-create.component.scss'
+  styleUrl: './drif-forecast-create.component.scss',
 })
 export class DrifForecastCreateComponent {
+  stepperOrientation: StepperOrientation = 'horizontal';
 
+  stepperSelectionChange(event: any) {}
+
+  goBack() {}
+
+  save() {}
 }
